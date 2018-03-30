17dc848bc0f0b34d4b5be270e19972231894e00b
wahao-mac <wuhaohao1993@outlook.com>
2018-03-30 14:13:33 +0800

    增加返回智能识别模式

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,8 @@ module.exports = {
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
+    // pass error which from wechat miniprogram system
+    'no-undef': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'space-before-function-paren': 0
